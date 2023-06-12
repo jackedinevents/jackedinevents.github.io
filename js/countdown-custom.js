da70361533e26b596d3da1a47526339b882bba6f
jackedinevents <99297144+jackedinevents@users.noreply.github.com>
2023-06-12 18:20:22 +0100

    Updated Company Address and dates to 2023.

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 8, 1, 20)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 8, 15, 20)}); // year, month, date, hour
         });
 });		
 
